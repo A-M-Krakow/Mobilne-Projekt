8d7b49b452230aa5eb8295de678c805b5f4308e6
Anna Madej <aniamadej123@gmail.com>
2017-05-17 11:04:00 +0200

    moved queries to different activity and made button to reach them in main activity

--- app/src/main/java/com/example/anna/mobilne_projekt/MainActivity.java
+++ app/src/main/java/com/example/anna/mobilne_projekt/MainActivity.java
@@ -1,173 +1,27 @@
 package com.example.anna.mobilne_projekt;
 
-import android.app.ListActivity;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import com.example.anna.mobilne_projekt.R;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
+public class MainActivity extends AppCompatActivity {
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-public class MainActivity extends ListActivity {
-
-    private String url = "http://a-m.netstrefa.pl/retrieve2.php";
-
-    private final String KEY_CLIENT = "klient";
-    private final String KEY_QUERY = "zapytanie";
-    private final String KEY_DESCRIPTION = "opis";
-
-    private final String KEY_ID = "IdZapytania";
-    private final String KEY_NAME = "ImieKlienta";
-    private final String KEY_SURNAME = "NazwiskoKlienta";
-    private final String KEY_EMAIL = "emailKlienta";
-    private final String KEY_PHONE = "telefonKlienta";
-
-    private final String KEY_AR_DATE = "dataPrzyj";
-    private final String KEY_DAYS = "iloscDni";
-    private final String KEY_ADULTS = "iloscDoroslych";
-    private final String KEY_BABIES = "iloscDzieci";
-    private final String KEY_TRAIN = "pkp";
-    private final String KEY_AIRPORT= "lotnisko";
-    private final String KEY_DOG= "pies";
-    private final String KEY_CLEANING= "sprzatanie";
-
-
-
-
-
-
+    Button queriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new ReadXMLTask().execute(url);
-}
-
-private class ReadXMLTask extends AsyncTask<String, Void, String> {
-    @Override
-
-    protected void onPreExecute() {}
-
-    protected String doInBackground(String... urls) {
-       String response = "";
-
-
-        try {
-            URL url = new URL(urls[0]);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.connect();
-            InputStream is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String s = "";
-
-            while ((s = br.readLine()) != null) {
-                response +=s;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return response;
+        queriesButton = (Button) findViewById(R.id.queriesButton);
     }
 
-    protected void onPostExecute(String result) {
-        XMLParser parser = new XMLParser();
-        Document doc = parser.getDomElement(result);
-        ArrayList<HashMap<String, String>> menuItems = new ArrayList<HashMap<String, String>>();
-
-        NodeList nl = doc.getElementsByTagName(KEY_QUERY);
-
-        for (int i = 0; i<nl.getLength(); i++) {
-            HashMap<String, String> map = new HashMap<String, String>();
-            Element e = (Element) nl.item(i);
-            map.put(KEY_QUERY, "Data przyjazdu: " + parser.getValue(e, KEY_AR_DATE) + " ilość dni:" +   parser.getValue(e, KEY_DAYS));
-            map.put(KEY_CLIENT, "Imię klienta: " + parser.getValue(e, KEY_NAME) + " Nazwisko klienta: " +   parser.getValue(e, KEY_SURNAME) + " Email klienta: " +   parser.getValue(e, KEY_EMAIL) + " Telefon klienta: " +   parser.getValue(e, KEY_PHONE) );
-            map.put(KEY_DESCRIPTION, "Ilość dorosłych: " + parser.getValue(e, KEY_ADULTS) + " Ilość dzieci: " + parser.getValue(e, KEY_BABIES) + " Pies: " + parser.getValue(e, KEY_DOG) + " PKP:" + parser.getValue(e, KEY_TRAIN) + " Samolot: " + parser.getValue(e, KEY_AIRPORT) + " Sprzątanie: " + parser.getValue(e, KEY_CLEANING) );
-
-
-
-
-
-            menuItems.add(map);
-
-        }
-        Log.i("ABC", menuItems.toString());
-        ListAdapter adapter = new SimpleAdapter(MainActivity.this, menuItems,R.layout.list_item, new String[] {KEY_QUERY, KEY_DESCRIPTION, KEY_CLIENT}, new int[] {R.id.zapytanie, R.id.opis, R.id.klient});
-
-        MainActivity.this.setListAdapter(adapter);
-
+    public void showQueries(View view)
+    {
+        Intent intent = new Intent(this, QueriesActivity.class);
+        startActivity(intent);
     }
-
-
-
-}
-
-    public class XMLParser{
-
-        public Document getDomElement(String xml){
-            Document doc = null;
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            try {
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                InputSource is = new InputSource();
-                is.setCharacterStream(new StringReader(xml));
-                doc = db.parse(is);
-            } catch (ParserConfigurationException e) {
-                e.printStackTrace();
-            } catch (SAXException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return doc;
-        }
-
-        public String getElementValue(Node elem) {
-            Node child;
-            if(elem != null) {
-                if (elem.hasChildNodes()) {
-                    for ( child = elem.getFirstChild(); child != null; child = child.getNextSibling()) {
-                        if (child.getNodeType()==Node.TEXT_NODE) {
-                            return child.getNodeValue();
-                        }
-                    }
-                }
-            }
-            return "";
-        }
-
-        public String getValue(Element item, String str) {
-            NodeList n = item.getElementsByTagName(str);
-            return this.getElementValue(n.item(0));
-        }
-    }
-
-
 }
